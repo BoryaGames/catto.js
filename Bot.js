14799fae0c7725738f79d2182546d418c5125696
BoryaGames <67229357+BoryaGames@users.noreply.github.com>
2023-02-01 20:33:54 +0500

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,5 +1,24 @@
 var events = require("events");
+var Discord = require("discord.js");
 if (typeof EventEmitter !== "undefined") {} else {
   var { EventEmitter } = events;
 }
-module.exports = class extends EventEmitter {};
+module.exports = class extends EventEmitter {
+  constructor(options) {
+    this.options = Object.assign({
+      "token": "",
+      "intents": 98045
+    }, options || {});
+    this.client = new Discord.Client({
+      "intents": new Discord.IntentsBitField(this.options.intents)
+    });
+  }
+  async run() {
+    return new Promise((res, rej) => {
+      this.client.login(this.options.token).then(() => {
+        this.emit("running");
+        res();
+      }).catch(rej);
+    });
+  }
+};
