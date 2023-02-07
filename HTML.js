ca43b743cba4dc163657b93570303c37eb940a02
BoryaGames <67229357+BoryaGames@users.noreply.github.com>
2023-02-07 14:12:55 +0500

    Update HTML.js

--- HTML.js
+++ HTML.js
@@ -1,8 +1,8 @@
 /**
-* Disables HTML tags in a given string.
-* @param {string} text - The string to disable HTML tags in.
-* @returns {string} - The string with HTML tags disabled.
-*/
+ * Disables HTML tags in a given string.
+ * @param {string} text - The string to disable HTML tags in.
+ * @returns {string} - The string with HTML tags disabled.
+ */
 function disable(text) {
   return text.split("<").join("&lt;").split(">").join("&gt;");
 }
