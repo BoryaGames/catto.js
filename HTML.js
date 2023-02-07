af3fe3dee424962eed73523d57210fff680baa27
BoryaGames <67229357+BoryaGames@users.noreply.github.com>
2023-02-07 13:55:51 +0500

    Update HTML.js

--- HTML.js
+++ HTML.js
@@ -1,5 +1,11 @@
+/**
+* Функция для отключения тегов HTML
+* @param {string} text - Текст, в котором нужно отключить теги HTML
+* @returns {string} Текст с отключенными тегами HTML
+*/
+function disable(text) {
+  return text.split("<").join("&lt;").split(">").join("&gt;");
+}
 module.exports = {
-  "disable": text => {
-    return text.split("<").join("&lt;").split(">").join("&gt;");
-  }
+  disable
 };
