4e078cee60420e9b9c2e9c6e6fb22f4effff8d42
BoryaGames <67229357+BoryaGames@users.noreply.github.com>
2023-02-07 14:19:43 +0500

    Update Base64.js

--- Base64.js
+++ Base64.js
@@ -1,8 +1,22 @@
+/**
+ * Encodes a string to base64.
+ * @function
+ * @param {string} text - The text to be encoded.
+ * @returns {string} - The encoded text in base64.
+ */
+function encode(text) {
+  return Buffer.from(text, "utf-8").toString("base64")
+}
+/**
+ * Decodes a base64 string.
+ * @function
+ * @param {string} text - The text to be decoded.
+ * @returns {string} - The decoded text.
+ */
+function decode(text) {
+  return Buffer.from(text, "base64").toString("utf-8")
+}
 module.exports = {
-  "encode": text => {
-    return Buffer.from(text, "utf-8").toString("base64")
-  },
-  "decode": text => {
-    return Buffer.from(text, "base64").toString("utf-8")
-  }
+  encode,
+  decode
 };
