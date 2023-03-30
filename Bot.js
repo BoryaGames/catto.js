6cc2766fb31110afcb709af1685a01d67eae085c
BoryaGames <67229357+BoryaGames@users.noreply.github.com>
2023-03-30 15:41:02 +0500

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -213,38 +213,7 @@ module.exports = class extends EventEmitter {
             "type": 1
           });
         } else {
-          var interaction = new Discord.BaseInteraction(this.client, req.body);
-          if (interaction.isCommand()) {
-            interaction = new Discord.CommandInteraction(this.client, req.body);
-            if (interaction.isChatInputCommand()) {
-              interaction = new Discord.ChatInputCommandInteraction(this.client, req.body);
-            }
-          } else if (interaction.isButton()) {
-            interaction = new Discord.ButtonInteraction(this.client, req.body);
-          }
-          interaction.reply = async options => {
-            if (this.deferred || this.replied) {
-              throw new Error("Already deferred or replied.");
-	    }
-            if (options.ephemeral !== null && options.ephemeral !== undefined) {
-              this.ephemeral = options.ephemeral;
-	    } else {
-              this.ephemeral = !1;
-            }
-            var messagePayload = null;
-            if (options instanceof Discord.MessagePayload) {
-              messagePayload = options;
-            } else {
-              messagePayload = Discord.MessagePayload.create(this, options);
-            }
-            var { body: data, files } = await messagePayload.resolveBody().resolveFiles();
-            res.json({
-              "type": 4,
-              data
-            });
-            this.replied = !0;
-          }
-          this.handleInteractionCreate(interaction);
+          this.client.actions.InteractionCreate.handle(req.body);
         }
       } else {
         res.status(401).end("Invalid request signature.");
