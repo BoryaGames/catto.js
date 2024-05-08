2b4158a260c1d7285b57da4d804297245fb5c1ef
BoryaGames <67229357+BoryaGames@users.noreply.github.com>
2024-05-08 18:33:06 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var mod = {};
 var version = Number(process.version.match(/^v(\d+\.\d+)/)[1]);
-var parts = ["random", "Server", "HTML", "request", "AuthClient", "utils", "GitHub", "Base64", "User", "Bitfield", "Application", "MessageBuilder", "TelegramBot", "TelegramMessage", "TelegramChannel", "TelegramMessageBuilder", "TelegramInteraction", "TelegramUser", "TelegramFile"];
+var parts = ["random", "Server", "HTML", "request", "AuthClient", "utils", "GitHub", "Base64", "User", "Bitfield", "Application", "TelegramBot", "TelegramMessage", "TelegramChannel", "TelegramMessageBuilder", "TelegramInteraction", "TelegramUser", "TelegramFile"];
 if (version >= 16.11) {
-  parts.push("Bot");
+  parts.push("Bot", "MessageBuilder");
 }
 parts.forEach(part => {
   mod[part] = require(`./${part}`);
