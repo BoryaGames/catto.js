f3f6fde7743f09f3f946c1ce58185222a788c3fd
BoryaGames <67229357+BoryaGames@users.noreply.github.com>
2024-05-08 19:53:51 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var mod = {};
 var version = Number(process.version.match(/^v(\d+\.\d+)/)[1]);
 var parts = ["random", "Server", "HTML", "request", "AuthClient", "utils", "GitHub", "Base64", "User", "Bitfield", "Application", "TelegramBot", "TelegramMessage", "TelegramChannel", "TelegramMessageBuilder", "TelegramInteraction", "TelegramUser", "TelegramFile"];
-if (version >= 16.11) {
+if (version >= 18.20) {
   parts.push("Bot", "MessageBuilder");
 }
 parts.forEach(part => {
