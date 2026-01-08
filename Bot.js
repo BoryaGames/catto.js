fe044f3e58616ea061f89d7aa18a0081a46c64ea
BoryaGames <67229357+BoryaGames@users.noreply.github.com>
2026-01-08 04:20:53 -0800

    Fix

--- Bot.js
+++ Bot.js
@@ -335,8 +335,8 @@ module.exports = class extends EventEmitter {
         fs.writeFileSync(this.options.auditFile, JSON.stringify(this.auditDatabase, null, 2));
       }
       if (!found) {
-        message.executor = message.author;
-        message.executorId = message.author.id;
+        message.deletedBy = message.author;
+        message.deletedById = message.author.id;
       }
       this.emit("messageDeleted", message);
     });
